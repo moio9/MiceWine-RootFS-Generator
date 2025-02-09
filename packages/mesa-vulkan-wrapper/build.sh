abed0294f9f1ce239abdda38c24d985c73962488
airidosas252 <airidaslideikis@gmail.com>
2025-02-09 13:15:56 +0200

    wrapper: switch to Jeez's AdrenoTools fork

--- packages/mesa-vulkan-wrapper/build.sh
+++ packages/mesa-vulkan-wrapper/build.sh
@@ -3,7 +3,7 @@ PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Android Wrapper"
 VK_DRIVER_LIB="libvulkan_wrapper.so"
 
-GIT_URL=https://github.com/xMeM/mesa
-GIT_COMMIT=d7a274ab77ecadb4b06175c38b726d24a6cb9210
+GIT_URL=https://gitlab.freedesktop.org/Pipetto-crypto/mesa
+GIT_COMMIT=bae70519ffa58c8e455a003bc453b60250c3047b
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
